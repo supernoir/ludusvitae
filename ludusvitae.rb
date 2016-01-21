4f8d77537b7510cb29f8f5735408bf663f6d4c5c
supernoir <kawohl.bentodesign@gmail.com>
2016-01-21 14:06:39 +0100

    Initializes repo and fills app with pseudocode

---
+++ ludusvitae.rb
@@ -0,0 +1,45 @@
+# LUDUS VITAE 0.0.I
+# rubynettes (c) 2016
+
+# Write down the rules!
+#   Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+#   Any live cell with two or three live neighbours lives on to the next generation.
+#   Any live cell with more than three live neighbours dies, as if by over-population.
+#   Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+
+# WHAT IS IT SUPPOSED TO DO?
+## - For each generation do
+## - create a board
+## - +1 Generation
+## - Fill the board with cells in accordance to rules (first gen: random)
+## - check alive / dead status
+## - update variables
+
+# WHAT ARE VARIABLES I WOULD NEED?
+
+is_alive = false
+position = 0
+generation = 0
+cells = []
+
+# Create a current board and a next board
+
+# Visualize the board in CLI -- hell yeah
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class currentBoard end
+# new.currentBoard
+
+
