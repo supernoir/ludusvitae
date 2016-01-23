fe5610eb71aa82d35709e2fca1453e22b489a7ba
supernoir <kawohl.bentodesign@gmail.com>
2016-01-23 23:35:13 +0100

    where everything becomes verbose

--- ludusvitae.rb
+++ ludusvitae.rb
@@ -29,6 +29,7 @@ end
 
 board(cells,cell)
 
+# IT WORKS THIS FAR
 
 def boardPrinter (cells)
 i = 0
@@ -40,10 +41,66 @@ i = 0
 end
 boardPrinter(cells)
 
+def aliveChecker(cells)
+rule1, rule2, rule3, rule4 = false
+i = 0
+for i in 1..10 do
+    puts "\n"
+    if cells[i] == "■"
+        puts i.to_s + ": It's alive"
+        isalive = true
+    else
+        puts i.to_s + ": It's dead"
+        isalive = false
+    end
+    
+# Rule 1
+    if cells[i-1] && cells[i+1] == "□"
+        puts "It's not gonna make it: " + cells[i-1] + ", " + cells[i+1] 
+        rule1 = false
+        isalive = false
+        puts "Rule 1 passed? " + rule1.to_s
+    else
+        puts "It might have a chance: " + cells[i-1] + ", " + cells[i+1]
+        rule1 = true
+        isalive = true
+        puts "Rule 1 passed? " + rule1.to_s       
+    end
+    
+# Rule 2
+    if (cells[i-1] || cells[-2]) && (cells[i+1] || cells[i+2]) == "□"
+        puts "Dead next because " + cells[i-2] + ", " + cells[i-1] + " and " + cells[i+1] + ", " + cells[i+2] 
+        rule2 = false
+        puts "Rule 2 passed? " + rule2.to_s  
+    else
+        puts "Alive next because " + cells[i-2] + ", " + cells[i-1] + " and " + cells[i+1] + ", " + cells[i+2] 
+        rule2 = true
+        puts "Rule 2 passed? " + rule2.to_s    
+    end
 
-puts cells[23]
-puts cells[99]
-puts cells.size
+# Rule 3
+    if (cells[i-3] || cells[i+3]) == "■" && rule2 == true 
+        rule3 = true
+        puts "Rule 3 passed? " + rule3.to_s  
+    else
+        rule3 = false
+        puts "Rule 3 passed? " + rule3.to_s    
+    end
+
+# Rule 4
+    if (cells[i-3] || cells[i+3]) == "■" && isalive == false 
+        rule4 = true
+        puts "Rule 4 passed? " + rule4.to_s  
+    else
+        rule4 = false
+        puts "Rule 4 passed? " + rule4.to_s    
+    end    
+    
+end    
+end
+
+aliveChecker(cells)
+
 
 
 
